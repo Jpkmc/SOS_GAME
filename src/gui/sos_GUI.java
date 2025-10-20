2b8d536f81a2407ff8e454aaba9df63cb8fd6ab1
Jpkmc <1234jacobpham@gmail.com>
2025-10-19 22:33:49 -0500

    working on the GUI for main game

---
+++ src/gui/sos_GUI.java
@@ -0,0 +1,43 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class sos_GUI extends JFrame {
+   private JCheckBox checkBox;
+   private JRadioButton rbSimple, rbGeneral;
+   private ButtonGroup radioGroup;
+   public sos_GUI() {
+       setTitle("SOS Board Game");
+       setSize(1000, 1000);
+       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       setLayout(new BorderLayout());
+       // Panel with a line border
+       JPanel mainPanel = new JPanel(new GridLayout(4, 1));
+       mainPanel.setBorder(BorderFactory.createTitledBorder("SOS Game"));
+       // Text (label)
+       mainPanel.add(new JLabel("Choose your board size: "));
+
+
+       // Checkbox
+       checkBox = new JCheckBox("Enable feature");
+       mainPanel.add(checkBox);
+
+
+       // Radio buttons
+       rbSimple = new JRadioButton("Simple", true);
+       rbGeneral = new JRadioButton("General");
+       radioGroup = new ButtonGroup();
+       radioGroup.add(rbSimple);
+       radioGroup.add(rbGeneral);
+       mainPanel.add(rbSimple);
+       mainPanel.add(rbGeneral);
+       add(mainPanel, BorderLayout.CENTER);
+   }
+   public static void main(String[] args) {
+       SwingUtilities.invokeLater(() -> {
+       	sos_GUI gui = new sos_GUI();
+           gui.setVisible(true);
+       });
+   }
+}
