db8bc5668ac3daed745c22c8d586c09e4ee933d1
Jpkmc <1234jacobpham@gmail.com>
2025-10-19 22:53:16 -0500

    Making a model and adding some logic to to the game

---
+++ src/model/sos_Main.java
@@ -0,0 +1,24 @@
+package model;
+
+public class sos_Main {
+    public enum Mode { Simple, General}\
+    public enum Player {Player1, Player2}
+
+    private int size;
+    private Mode mode;
+    private char board[][];
+    private Player currentPlayer;
+
+
+    public void SOS_Main(int size, Mode mode){
+        if(size < 3){
+            throw new IllegalArgumentException("Board size must be greater than or eqaul to 3")
+        }
+        this.size = size;
+        this.mode = mode;
+        this.board = new char[size][size];
+        this.currentPlayer = Player.Player1;
+    }
+
+    
+}
