71920073fe8255548f696a225510d85e1277ddc4
Jpkmc <jacobpham22@gmail.com>
2025-11-17 22:23:11 -0600

    wokring on building the first function on sm for the computer

--- src/main/java/model/sos_computerSM.java
+++ src/main/java/model/sos_computerSM.java
@@ -1,11 +1,14 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Computer AI strategy for Simple Mode
  * Focuses on forming one SOS to win the game
  */
 public class sos_computerSM {
-    private sos_Model model;
+    final private sos_Model model;
     
     public sos_computerSM(sos_Model model) {
         this.model = model;
@@ -16,12 +19,21 @@ public class sos_computerSM {
      * Prioritizes winning moves, then blocking opponent
      */
     public int[] findBestMove() {
+        int boardSize = model.getBoard().length;
+        
+        for (int row = 0; row < boardSize; row++) {
+            for (int col = 0; col < boardSize; col++) {
+                if (model.cellEmpty(row, col)) {
+                    for (char letter : new char[]{'S', 'O'}) {
+                        if (isWinningMove(row, col, letter)) {
+                            return new int[]{row, col, letter == 'S' ? 1 : 2};
+                        }
+                    }
+                }
+            }
+        }
         return null;
     }
-    
-    /**
-     * Checks if this move wins the game immediately
-     */
     public boolean isWinningMove(int row, int col, char letter) {
         return false;
     }
@@ -39,5 +51,5 @@ public class sos_computerSM {
     public int evaluateMove(int row, int col, char letter) {
         return 0;
     }
-    
+      
 }
